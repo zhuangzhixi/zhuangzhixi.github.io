1b66d72786b96394fc23c4f2c7cb7ed26216fec7
zhuangzhixi <2018418308@qq.com>
2022-11-12 13:39:24 +0800

    second commit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -32,7 +32,7 @@ module.exports = {
     // 默认值是 true 。设置为 false 来禁用所有页面的 上一篇 链接
     prevLinks: true,
     // 导航栏显示 gitee 仓库
-    repo: 'https://gitee.com/brucecai55520/bruceblog',
-    repoLabel: 'Gitee',
+    repo: 'https://github.com/zhuangzhixi/zhuangzhixi.github.io/tree/master',
+    repoLabel: 'GitHub',
   },
 }
